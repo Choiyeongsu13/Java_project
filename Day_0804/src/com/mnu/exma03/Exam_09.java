a3a27c060f392539d4caa3e0ef8bd45f213f3da0
컴퓨터 <choiyeongsu0813@gmail.com>
2026-08-04 12:11:42 +0900

    List,Map,Set,Stack,Queue

--- Day_0804/src/com/mnu/exma03/Exam_09.java
+++ Day_0804/src/com/mnu/exma03/Exam_09.java
@@ -8,14 +8,14 @@ public class Exam_09 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Queue<Integer>queue = new LinkedList<Integer>();
-		queue.offer(100);
+		queue.offer(100); //삽입
 		queue.offer(200);
 		queue.offer(300);
 		queue.offer(400);
 		System.out.println("큐 객체 수 : " + queue.size());
 		
 		while(!queue.isEmpty()) {
-			int a = queue.poll();
+			int a = queue.poll(); //제거
 			System.out.println("꺼낸 객체 : " + a);
 			System.out.println("큐 객체 수 : " + queue.size());
 			
